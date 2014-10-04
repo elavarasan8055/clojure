fc4503349b1c026f29abfba15d457c8a779dd98d
elavarasan8055 <elavarasan8055@gmail.com>
2014-10-04 15:19:09 +0530

    Delete arrange.clj

--- arrange.clj
+++
@@ -1,7 +0,0 @@
-(defn arrange 
-   ([arr] (let [cnt (/ (count arr) 2)] (arrange (take cnt arr) (drop cnt arr))))
-   ([a b] (if (= (count a) 1) (concat a b) 
-                             (concat (arrange (take (/ (count a) 2) a) (take (/ (count a) 2) b)) (arrange (drop (/ (count a) 2) a) (drop (/ (count b) 2) b))))))
-
-
-
